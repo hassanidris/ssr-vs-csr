c5e087348d0dae5ccec552be95fa9221104c6bb2
Hassen Ahmed <hassan.creative@gmail.com>
2024-01-26 11:44:40 +0100

    initial commit

--- src/app/ssr/page.js
+++ src/app/ssr/page.js
@@ -6,13 +6,25 @@ import { imageUrlToBase64 } from "@/helpers";
 
 async function getData() {
   // Exercise 1 - Fetch the data here...
-  //
-  //
+
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1&thumbs=true`,
+    { cache: "no-cache" }
+  );
+
+  if (!response.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  const [json] = await response.json();
+
   // Exercise 2 - Optimize the image loading here...
-  //
-  //
+
+  [json].image = imageUrlToBase64(json.url);
+
   // Uncomment this when you're ready
-  // return json
+  return json;
 }
 
 export default async function SSR() {
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_KEY = "DEMO_KEY";
+export const API_KEY = "6KlHNqkt4qsKyQrrLVYo8ynTpRc3sb8NViVJdWMl";
